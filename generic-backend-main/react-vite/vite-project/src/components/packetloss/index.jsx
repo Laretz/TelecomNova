bfc06192cd80a841b972fe75fefabfa1599b56f4
Renato Ramon <renatophoenix123@gmail.com>
2024-10-19 18:01:43 -0300

    feat(PacketLossGraphComponent): add um componente, para exibir em grafico a quantidade de vezes e cada cliente teve perda de pacote

--- generic-backend-main/react-vite/vite-project/src/components/packetloss/index.jsx
+++ generic-backend-main/react-vite/vite-project/src/components/packetloss/index.jsx
@@ -1,34 +1,60 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 import { Chart } from "react-google-charts";
 
-
 const PacketLossGraphComponent = () => {
-    
-const data = [
-    ["Year", "Sales", "Expenses"],
-    ["2014", 1000, 400],
-    ["2015", 1170, 460],
-    ["2016", 660, 1120],
-    ["2017", 1030, 540],
-  ];
-  
-  const options = {
-    chart: {
-      title: "Company Performance",
-      subtitle: "Sales and Expenses over the Years",
+  const [data, setData] = useState([["Cliente", "Quantidade de packetLoss"]]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("http://localhost:3333/packetLossAvgClientes");
+        const resultados = await response.json();
+
+        // Formatar os dados recebidos
+        const formattedData = resultados.map(item => [
+          item.customerName || "Unknown", // Nome do cliente
+          Number(item.packetLoss10Count) || 0, // Contagem de Packet Loss
+        ]);
+
+        // Substituir os dados anteriores, sem duplicação
+        setData([["Cliente", "Quantidade de packetLoss"], ...formattedData]);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []); // executa apenas uma vez, ao montar o componente
+
+
+  const options = {                    // options do grafico
+    hAxis: {
+      title: 'Clientes',
+    },
+    vAxis: {
+      title: 'Quantidade de Packet Loss',
+    },
+    legend: { position: 'none' }, 
+    annotations: {
+      alwaysOutside: true, 
+      textStyle: {
+        color: '#000', 
+        fontSize: 12, 
+      },
     },
   };
 
-    return (
-        <div>
-            <Chart
-                chartType="Bar"
-                data={data}
-                options={options}
-                
-            />
-        </div>
+  return (
+    <div>
+      <Chart
+        chartType="Bar" 
+        width="100%"
+        height="280px"
+        data={data}
+        options={options}
+      />
+    </div>
   );
-}
+};
 
-export default PacketLossGraphComponent
+export default PacketLossGraphComponent;
