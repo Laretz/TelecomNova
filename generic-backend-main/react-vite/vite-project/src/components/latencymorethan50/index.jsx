fe321f614d39eda8691647e71c6edc777e91529d
Renato Ramon <renatophoenix123@gmail.com>
2024-10-19 18:00:21 -0300

    feat(LatencyMoreThan50GraphComponent): add novo componente para exibir em gráfico, a porcentagem de metricas onde a latência é maior que 50 para cada cliente

--- generic-backend-main/react-vite/vite-project/src/components/latencymorethan50/index.jsx
+++ generic-backend-main/react-vite/vite-project/src/components/latencymorethan50/index.jsx
@@ -1,9 +1,64 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { Chart } from "react-google-charts";
 
 const LatencyMoreThan50GraphComponent = () => {
-  return (
-    <div>Oi</div>
-  )
-}
+  const [data, setData] = useState([["Cliente", "Quantidade"]]);   // cabeçalho do grafico
+  const [loading, setLoading] = useState(true);
 
-export default LatencyMoreThan50GraphComponent
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("http://localhost:3333/latencyMoreThan50Clientes");
+        const result = await response.json();
+
+        
+        
+        const clientData = {};
+
+        // cotar quantos items tem para cada cliente 
+        result.forEach((item) => {
+          const clientName = item.customer.name;
+          if (!clientData[clientName]) {
+            clientData[clientName] = 0;
+          }
+          clientData[clientName] += 1;
+        });
+
+        // formata data para p piechart
+        const formattedData = [["Cliente", "Quantidade"]];
+        Object.keys(clientData).forEach((client) => {
+          formattedData.push([client, clientData[client]]);
+        });
+
+        setData(formattedData);  // define os dados para o grafico
+      } catch (error) {
+        console.error("Erro ao carregar dados:", error);
+      } finally {
+        setLoading(false);  
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  const options = {    //options do grafico
+    title: "Métricas que a latência foi acima de 50ms por Cliente",
+    is3D: true, 
+  };
+
+  if (loading) {
+    return <div>Carregando dados...</div>;
+  }
+
+  return (
+    <Chart
+      chartType="PieChart"
+      data={data}
+      options={options}
+      width={"100%"}
+      height={"280"}
+    />
+  );
+};
+
+export default LatencyMoreThan50GraphComponent;
